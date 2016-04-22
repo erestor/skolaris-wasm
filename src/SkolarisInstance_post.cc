43f47750b3e75c9e24d5282d9a7fb15b4ba89aab
Martin Klemsa <m.klemsa@gmail.com>
2016-04-22 09:10:48 +0200

    updated version

--- src/SkolarisInstance_post.cc
+++ src/SkolarisInstance_post.cc
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-const string SKOLARIS_VERSION("1.20.0"
+const string SKOLARIS_VERSION("1.21.0"
 #ifdef DEBUG
 	"debug"
 #endif
 );
 
 const string SKOLARIS_VERSION_MAJOR("1");
-const string SKOLARIS_VERSION_MINOR("20");
+const string SKOLARIS_VERSION_MINOR("21");
 const string SKOLARIS_VERSION_PATCH("0");
 
 void SkolarisInstance::post_complete(int requestId)
