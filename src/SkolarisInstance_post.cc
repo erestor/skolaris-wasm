275139cb8605a2a9ecef1024e3d2cf131b8b77b5
Martin Klemsa <m.klemsa@gmail.com>
2017-06-27 15:29:44 +0200

    Prevented subgroups with too few lessons from breaking MinPeriodsPerDay
    - by default

--- src/SkolarisInstance_post.cc
+++ src/SkolarisInstance_post.cc
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const string SKOLARIS_VERSION("1.28.0"
+const string SKOLARIS_VERSION("1.28.1"
 #ifdef DEBUG
 	"debug"
 #endif
@@ -12,7 +12,7 @@ const string SKOLARIS_VERSION("1.28.0"
 
 const string SKOLARIS_VERSION_MAJOR("1");
 const string SKOLARIS_VERSION_MINOR("28");
-const string SKOLARIS_VERSION_PATCH("0");
+const string SKOLARIS_VERSION_PATCH("1");
 
 void SkolarisInstance::post_complete(int requestId)
 {
