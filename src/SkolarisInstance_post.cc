863e19087b570ef5e08c8491b33075992afb0006
Martin Klemsa <m.klemsa@gmail.com>
2017-07-22 16:26:37 +0200

    Changed block day occurrence penalty calculation

--- src/SkolarisInstance_post.cc
+++ src/SkolarisInstance_post.cc
@@ -4,15 +4,15 @@
 
 using namespace std;
 
-const string SKOLARIS_VERSION("2.4.2"
+const string SKOLARIS_VERSION("2.5.0"
 #ifdef DEBUG
 	"debug"
 #endif
 );
 
 const string SKOLARIS_VERSION_MAJOR("2");
-const string SKOLARIS_VERSION_MINOR("4");
-const string SKOLARIS_VERSION_PATCH("2");
+const string SKOLARIS_VERSION_MINOR("5");
+const string SKOLARIS_VERSION_PATCH("0");
 
 void SkolarisInstance::post_complete(int requestId)
 {
