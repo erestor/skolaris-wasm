17cb514f6f89bef4c246f7e59514eed2397649fc
Martin Klemsa <m.klemsa@gmail.com>
2017-09-13 09:03:44 +0200

    Added XHSTT block spread constraint

--- src/SkolarisInstance_post.cc
+++ src/SkolarisInstance_post.cc
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const string SKOLARIS_VERSION("2.7.3"
+const string SKOLARIS_VERSION("2.7.4"
 #ifdef DEBUG
 	"debug"
 #endif
@@ -12,7 +12,7 @@ const string SKOLARIS_VERSION("2.7.3"
 
 const string SKOLARIS_VERSION_MAJOR("2");
 const string SKOLARIS_VERSION_MINOR("7");
-const string SKOLARIS_VERSION_PATCH("3");
+const string SKOLARIS_VERSION_PATCH("4");
 
 void SkolarisInstance::post_complete(int requestId)
 {
