97b6e8e7f50dccc5126914472f7f6a5711137485
Martin Klemsa <m.klemsa@gmail.com>
2019-01-15 14:27:32 +0100

    Update

--- src/SkolarisInstance_post.cc
+++ src/SkolarisInstance_post.cc
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const string SKOLARIS_VERSION("9.0.0"
+const string SKOLARIS_VERSION("9.0.3"
 #ifdef DEBUG
 	"debug"
 #endif
@@ -12,7 +12,7 @@ const string SKOLARIS_VERSION("9.0.0"
 
 const string SKOLARIS_VERSION_MAJOR("9");
 const string SKOLARIS_VERSION_MINOR("0");
-const string SKOLARIS_VERSION_PATCH("0");
+const string SKOLARIS_VERSION_PATCH("3");
 
 void SkolarisInstance::post_complete(int requestId)
 {
