0912341ab1344df3cdd7b357c5e6f9910557acef
Martin Klemsa <m.klemsa@gmail.com>
2020-07-13 15:09:32 +0200

    Update

--- src/SkolarisInstance_post.cc
+++ src/SkolarisInstance_post.cc
@@ -8,7 +8,7 @@
 using namespace boost::property_tree;
 using namespace std;
 
-const string SKOLARIS_VERSION("9.17.3"
+const string SKOLARIS_VERSION("9.17.4"
 #ifdef DEBUG
 	"debug"
 #endif
@@ -16,7 +16,7 @@ const string SKOLARIS_VERSION("9.17.3"
 
 const string SKOLARIS_VERSION_MAJOR("9");
 const string SKOLARIS_VERSION_MINOR("17");
-const string SKOLARIS_VERSION_PATCH("3");
+const string SKOLARIS_VERSION_PATCH("4");
 
 void SkolarisInstance::postMessage(const ptree &data) const
 {
