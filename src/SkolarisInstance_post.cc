e4a319e378d0ea9d295ce29d5084fbd32f35ec08
Martin Klemsa <m.klemsa@gmail.com>
2021-08-18 13:59:36 +0200

    Update

--- src/SkolarisInstance_post.cc
+++ src/SkolarisInstance_post.cc
@@ -8,7 +8,7 @@ using namespace std;
 
 const string SKOLARIS_VERSION_MAJOR{"10"};
 const string SKOLARIS_VERSION_MINOR{"7"};
-const string SKOLARIS_VERSION_PATCH{"0b"};
+const string SKOLARIS_VERSION_PATCH{"1"};
 
 const string SKOLARIS_VERSION = SKOLARIS_VERSION_MAJOR + "." + SKOLARIS_VERSION_MINOR + "." + SKOLARIS_VERSION_PATCH
 #ifdef DEBUG
